9f29427023a8c6a27f1cdd29cf269c090f8bc144
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-06-12 14:43:22 +0300

    commit tree 7a6b7cae4d4f58d76d1cbbdda3bdfd80e5de6811

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries=batteryBatches.reduce((accumulator,currentValue) => accumulator + currentValue);
+console.log(totalBatteries)
+// const totalBatteries=(accumulator,batteryBatches) =>{
+//     console.log(accumulator,batteryBatches)
+//     let total="sum of batteryBatches";
+//     return accumulator +=total
+// }
+// let total=batteryBatches.reduce(reducer,4)
+
+// console.log(totalBatteries)
+
+
+
+
+// //    const reducer=(accumulator, item) =>{
+// //     console.log(accumulator, item)
+// //     let total= item.price * item.price
+// //     return accumulator += total
+// //    }
+
+// //    let total= books.reduce(reducer,0)
